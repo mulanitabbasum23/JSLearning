02ee4b2fc9fab1db24f8b3e2ea2def817eab95a0
mulanitabbasum23 <mulanitabbasum23@gmail.com>
2023-08-03 14:57:54 +0530

    Basic programs on set, map and object
    Basic programs on set, map and object

---
+++ 09_object.js
@@ -0,0 +1,14 @@
+let personTM = {
+    empId: 2233,
+    state: "MH",
+    designation: "Tech Lead",
+    city: "Pune",
+    isMarried: true,
+    empId: 2233
+}
+console.log(personTM);
+
+console.log(typeof personTM);
+
+console.log(`Emp Id: ${personTM.empId}`); // Dot Notation
+console.log(`Emp city: ${personTM["city"]}`);
