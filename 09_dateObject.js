47e77bc83e11d8eb29065bbe64cf4fd1dd6961da
mulanitabbasum23 <mulanitabbasum23@gmail.com>
2023-08-21 11:28:47 +0530

    Basic program by using Date function
    Basic program by using Date function

---
+++ 09_dateObject.js
@@ -0,0 +1,13 @@
+let date = new Date();
+console.log(date);
+console.log(date.getDate());
+console.log(date.toDateString());
+console.log(date.toTimeString());
+var dt = new Date("Aug 21, 2023 11:24:06");
+console.log(dt);
+const d = new Date("2015-03-24");
+console.log(d);
+const dd = new Date("05/23/1997");
+console.log(dd);
+const ddd = new Date("21 Oct 2020");
+console.log(ddd);
